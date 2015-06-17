3c1a23504dd97c55bdc00d72ead82d1acfd85d8f
Patrick Simon <pas.freelance@googlemail.com>
2015-06-17 09:45:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+SimpleFlake - It's new $module
+
+# SYNOPSIS
+
+    use SimpleFlake;
+
+# DESCRIPTION
+
+SimpleFlake is ...
+
+# LICENSE
+
+Copyright (C) Patrick Simon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Patrick Simon <pas.freelance@googlemail.com>
---
+++ lib/SimpleFlake.pm
@@ -0,0 +1,39 @@
+package SimpleFlake;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SimpleFlake - It's new $module
+
+=head1 SYNOPSIS
+
+    use SimpleFlake;
+
+=head1 DESCRIPTION
+
+SimpleFlake is ...
+
+=head1 LICENSE
+
+Copyright (C) Patrick Simon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Patrick Simon E<lt>pas.freelance@googlemail.comE<gt>
+
+=cut
+
